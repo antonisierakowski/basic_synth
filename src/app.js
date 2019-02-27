2f6a7b7ce494176bfc05f0f4f449f71e2bee8c3f
Antoni Sierakowski <antonisierakowski@gmail.com>
2019-02-27 16:05:26 +0100

    panel placeholders

--- src/app.js
+++ src/app.js
@@ -8,34 +8,56 @@ import "./styles/main.scss";
 
 $( () => {
       const $body = $('body');
-      let curOct = 4;
-      // var listener = new window.keypress.Listener();
+      let curOct = 2;
+      let synth = new Tone.PolySynth(10)
+      console.log(synth)
+      let chorus = new Tone.Chorus(1.5, 3.5, 0.7);
+      let reverb = new Tone.Reverb(20);
 
 
+      synth.chain(chorus, reverb, Tone.Master);
+      reverb.generate();
+      reverb.wet.value = (0.2);
 
-      let synth = new Tone.Synth();
 
-      synth.chain(Tone.Master)
+      handleKeyboard(curOct)
 
-      addPressEvent($keys.cKey, 65, `C${curOct}`)
-      addPressEvent($keys.dKey, 83, `D${curOct}`)
-      addPressEvent($keys.eKey, 68, `E${curOct}`)
-      addPressEvent($keys.fKey, 70, `F${curOct}`)
-      addPressEvent($keys.gKey, 71, `G${curOct}`)
-      addPressEvent($keys.aKey, 72, `A${curOct}`)
-      addPressEvent($keys.bKey, 74, `B${curOct}`)
-      addPressEvent($keys.c2Key, 75, `C${curOct+1}`)
-      addPressEvent($keys.cSharpKey, 87, `C#${curOct}`)
-      addPressEvent($keys.dSharpKey, 69, `D#${curOct}`)
-      addPressEvent($keys.fSharpKey, 84, `F#${curOct}`)
-      addPressEvent($keys.gSharpKey, 89, `G#${curOct}`)
-      addPressEvent($keys.aSharpKey, 85, `A#${curOct}`)
+      function handleKeyboard(oct) {
+            console.log(curOct)
+            $body.off('keydown');
+            $body.off('keyup');
+            addPressEvent($keys.cKey, 65, `C${oct}`)
+            addPressEvent($keys.dKey, 83, `D${oct}`)
+            addPressEvent($keys.eKey, 68, `E${oct}`)
+            addPressEvent($keys.fKey, 70, `F${oct}`)
+            addPressEvent($keys.gKey, 71, `G${oct}`)
+            addPressEvent($keys.aKey, 72, `A${oct}`)
+            addPressEvent($keys.bKey, 74, `B${oct}`)
+            addPressEvent($keys.c2Key, 75, `C${oct+1}`)
+            addPressEvent($keys.cSharpKey, 87, `C#${oct}`)
+            addPressEvent($keys.dSharpKey, 69, `D#${oct}`)
+            addPressEvent($keys.fSharpKey, 84, `F#${oct}`)
+            addPressEvent($keys.gSharpKey, 89, `G#${oct}`)
+            addPressEvent($keys.aSharpKey, 85, `A#${oct}`)
+
+            $body.on('keyup', () => {
+                  if (event.keyCode === 90) {
+                        curOct--;
+                        handleKeyboard(curOct)
+                  }
+                  if (event.keyCode === 88) {
+                        curOct++;
+                        handleKeyboard(curOct)
+                  }
+            })
+      }
 
       function addPressEvent(element, keycode, note) {
+
             $body.on('keydown', (eventDown) => {
                   if (eventDown.keyCode === keycode) {
-                        synth.triggerAttackRelease(note)
-                        $(element).addClass('depressed')
+                        synth.triggerAttackRelease(note, '8n');
+                        $(element).addClass('depressed');
                         $body.on('keyup', (eventUp) => {
                               if (eventUp.keyCode === keycode) {
                                     $(element).removeClass('depressed')
@@ -44,21 +66,6 @@ $( () => {
                   }
             })
       }
-
-
-   
-
-      
-
-  
-
-
-
-      // }
-
-      // function playNote(); {
-
-      // }
   
 })
 
--- webpack.config.js
+++ webpack.config.js
@@ -127,7 +127,7 @@ module.exports = function(env) {
 
   if (isDev) {
     config.devServer = {
-      port: 8080,
+      port: 8082,
       progress: true,
       overlay: true,
       historyApiFallback: true
